3a8b2b502c544e8429a6f4d18664c8a4199303ef
Anwesh43 <anweshthecool0@gmail.com>
2018-10-06 11:49:17 +0530

    adding ThreeLineStepView create method

--- app/src/main/java/com/anwesh/uiprojects/linkedthreelinestepview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedthreelinestepview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedthreelinestepview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.threelinestepview.ThreeLineStepView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        ThreeLineStepView.create(this)
     }
 }
