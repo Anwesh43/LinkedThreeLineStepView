a7a569a51c82f8735419af907c8bc10dc4deda34
Anwesh43 <anweshthecool0@gmail.com>
2018-10-06 10:59:17 +0530

    adding libraries and defining nodes

--- threelinestepview/src/main/java/com/anwesh/uiprojects/threelinestepview/ThreeLineStepView.kt
+++ threelinestepview/src/main/java/com/anwesh/uiprojects/threelinestepview/ThreeLineStepView.kt
@@ -3,3 +3,13 @@ package com.anwesh.uiprojects.threelinestepview
 /**
  * Created by anweshmishra on 06/10/18.
  */
+
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.content.Context
+
+val nodes : Int = 5
