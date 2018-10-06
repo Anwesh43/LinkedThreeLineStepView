170215ea29160df2a8f59d329d35b5217473d719
Anwesh43 <anweshthecool0@gmail.com>
2018-10-06 11:31:24 +0530

    adding threelinestep class

--- threelinestepview/src/main/java/com/anwesh/uiprojects/threelinestepview/ThreeLineStepView.kt
+++ threelinestepview/src/main/java/com/anwesh/uiprojects/threelinestepview/ThreeLineStepView.kt
@@ -134,4 +134,26 @@ class ThreeLineStepView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class ThreeLineStep(var i : Int) {
+        private var curr : TLSNode = TLSNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            curr.update {i, scl ->
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(i, scl)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
